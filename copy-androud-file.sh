dcb57ea6d7795e8d1938df6d57627734b8f327ef
young.un <duddns@gmail.com>
2016-02-16 10:52:26 +0900

    Update copy-androud-file.sh

--- copy-androud-file.sh
+++ copy-androud-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/Library/Android/sdk/platform-tools/adb shell "mkdir -p '/sdcard/Backup/$2'"
-~/Library/Android/sdk/platform-tools/adb shell "run-as $1 cat '/data/data/$1/$2/$3' > '/sdcard/Backup/$2/$3'"
-~/Library/Android/sdk/platform-tools/adb pull "/sdcard/Backup/$2/$3"
-~/Library/Android/sdk/platform-tools/adb shell "rm -r /sdcard/Backup/$2"
+adb shell "mkdir -p '/sdcard/Backup/$2'"
+adb shell "run-as $1 cat '/data/data/$1/$2/$3' > '/sdcard/Backup/$2/$3'"
+adb pull "/sdcard/Backup/$2/$3"
+adb shell "rm -r /sdcard/Backup/$2"
